e9963a2ae174d2f090b064a3e0a56191bed052aa
JafarAbdi <cafer.abdi@gmail.com>
2022-04-07 19:07:01 +0300

    Revert custom lspkind comparator

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -1,57 +1,4 @@
 -- nvim-cmp setup
-
--- Same as the default compare.kind with field set to 25
-local completion_item_kind = {}
-completion_item_kind.Text = 1
-completion_item_kind.Method = 2
-completion_item_kind.Function = 3
-completion_item_kind.Constructor = 4
-completion_item_kind.Field = 26 -- Default 5
-completion_item_kind.Variable = 6
-completion_item_kind.Class = 7
-completion_item_kind.Interface = 8
-completion_item_kind.Module = 9
-completion_item_kind.Property = 10
-completion_item_kind.Unit = 11
-completion_item_kind.Value = 12
-completion_item_kind.Enum = 13
-completion_item_kind.Keyword = 14
-completion_item_kind.Snippet = 15
-completion_item_kind.Color = 16
-completion_item_kind.File = 17
-completion_item_kind.Reference = 18
-completion_item_kind.Folder = 19
-completion_item_kind.EnumMember = 20
-completion_item_kind.Constant = 21
-completion_item_kind.Struct = 22
-completion_item_kind.Event = 23
-completion_item_kind.Operator = 24
-completion_item_kind.TypeParameter = 25
-completion_item_kind = vim.tbl_add_reverse_lookup(completion_item_kind)
-
--- https://github.com/hrsh7th/nvim-cmp/issues/156#issuecomment-916338617
-local lspkind_comparator = function(entry1, entry2)
-  local kind1 = entry1:get_kind()
-  kind1 = kind1 == completion_item_kind.Text and 100 or kind1
-  local kind2 = entry2:get_kind()
-  kind2 = kind2 == completion_item_kind.Text and 100 or kind2
-  if kind1 ~= kind2 then
-    if kind1 == completion_item_kind.Snippet then
-      return true
-    end
-    if kind2 == completion_item_kind.Snippet then
-      return false
-    end
-    local diff = kind1 - kind2
-    if diff < 0 then
-      return true
-    elseif diff > 0 then
-      return false
-    end
-  end
-end
-
--- local luasnip = require("luasnip")
 local cmp = require("cmp")
 local compare = require("cmp.config.compare")
 
@@ -143,10 +90,10 @@ cmp.setup({
     comparators = {
       compare.offset,
       compare.exact,
-      compare.score,
+      require("clangd_extensions.cmp_scores"),
+      -- compare.score,
       compare.recently_used,
-      lspkind_comparator,
-      -- compare.kind,
+      compare.kind,
       compare.sort_text,
       compare.length,
       compare.order,
