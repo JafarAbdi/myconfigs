f56e34dbf0d9d146d298e982176f8312c438d97b
JafarAbdi <cafer.abdi@gmail.com>
2022-04-07 05:22:57 +0300

    Add fzfmenu

--- scripts/linkgrabber.sh
+++ scripts/linkgrabber.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 regex='(((http|https|ftp|gopher)|mailto)[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
-url=$(grep -Po "$regex" | uniq | dmenu -p "Go:" -w "$WINDOWID") || exit 0
-$BROWSER "$url" > /dev/null
+url=$(grep -Po "$regex" | uniq | fzfmenu --preview="" | awk '{print $1}' | xargs -d '\n' -I{} -n1 -r xdg-open '{}') || exit 0
