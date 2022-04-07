5560681c07be571529cce628ab63fcae9f9e4135
JafarAbdi <cafer.abdi@gmail.com>
2022-04-07 06:48:19 +0300

    Use tmux popup for linkgrabber

--- scripts/linkgrabber.sh
+++ scripts/linkgrabber.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+input=$(mktemp -u --suffix .linkgrabber.input)
+output=$(mktemp -u --suffix .linkgrabber.output)
+mkfifo $input
+mkfifo $output
+chmod 600 $input $output
+
 regex='(((http|https|ftp|gopher)|mailto)[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
-url=$(grep -Po "$regex" | uniq | fzfmenu --preview="" | awk '{print $1}' | xargs -d '\n' -I{} -n1 -r xdg-open '{}') || exit 0
+
+tmux popup -E "cat $input | grep -Po '$regex' | uniq | fzf --preview='' | tee $output" & disown
+
+# handle ctrl+c outside child terminal window
+trap "kill $! 2>/dev/null; rm -f $input $output" EXIT
+
+cat > $input
+cat $output | awk '{print $1}' | xargs -d '\n' -I{} -n1 -r xdg-open '{}' > /dev/null
