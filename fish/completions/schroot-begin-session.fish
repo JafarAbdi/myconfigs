e114c43c5edcf31a90cb6f4bd144ced8f01e5578
JafarAbdi <cafer.abdi@gmail.com>
2022-05-23 15:41:15 +0300

    Fix schroot not instaleld

--- fish/completions/schroot-begin-session.fish
+++ fish/completions/schroot-begin-session.fish
@@ -1,2 +1,4 @@
-set -l chroots (schroot --list --all)
-complete -c schroot-begin-session -x -a "$chroots"
+if command -v schroot &> /dev/null
+  set -l chroots (schroot --list --all)
+  complete -c schroot-begin-session -x -a "$chroots"
+end
