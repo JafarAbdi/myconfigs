cdf7d2938b1877ec90c50fe5fea7cd7dccbd3a09
JafarAbdi <cafer.abdi@gmail.com>
2022-08-02 17:12:49 +0300

    NVim: Fix efm's root_dir

--- nvim/lua/configs/lsp/servers/efm.lua
+++ nvim/lua/configs/lsp/servers/efm.lua
@@ -10,4 +10,7 @@ lspconfig.efm.setup({
     -- "6",
   },
   filetypes = { "lua", "cmake", "json", "markdown", "rst", "sh", "tex", "yaml", "python" },
+  root_dir = function(dir)
+    return lspconfig.util.find_git_ancestor(dir) or vim.loop.cwd()
+  end,
 })
