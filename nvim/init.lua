4f05fc8dbe5dfaef2d5fc13be8b1ca10201b61e0
JafarAbdi <cafer.abdi@gmail.com>
2022-09-14 18:03:47 +0300

    NVim: calling nvim from terminal will open the files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,25 @@
-vim.cmd([[
-if exists('$NVIMRUNNING')
-    "can't run nvim inside terminal emulator
-    qall!
+if vim.fn.exists("$NVIMRUNNING") == 1 then
+  -- can't run nvim inside terminal emulator
+  vim.fn.jobstart({
+    "nvim",
+    -- No need to load plugins
+    "-u",
+    "NONE",
+    "--server",
+    vim.env.NVIMRUNNING,
+    "--remote",
+    -- Convert all paths to absolute form since the files will be opened w.r.t. the servers cwd
+    vim.fn.join(
+      vim.tbl_map(function(e)
+        return vim.fn.fnamemodify(e, ":p")
+      end, vim.fn.argv()),
+      " "
+    ),
+  })
+  vim.cmd("qall!")
 else
-    let $NVIMRUNNING = 1
-endif
-]])
+  vim.fn.setenv("NVIMRUNNING", vim.api.nvim_get_vvar("servername"))
+end
 
 require("configs.options")
 require("configs.plugins")
