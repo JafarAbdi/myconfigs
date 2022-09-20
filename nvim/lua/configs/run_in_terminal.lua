58bbdc8d0cac3a4b28da69443b326c9f0812099c
JafarAbdi <cafer.abdi@gmail.com>
2022-09-20 15:11:34 +0300

    NVim: Fix run_in_terminal not deleting previous terminal

--- nvim/lua/configs/run_in_terminal.lua
+++ nvim/lua/configs/run_in_terminal.lua
@@ -4,8 +4,15 @@ return function(cmd, args, opts)
     return
   end
 
+  local terminal_name = "[Run Terminal]"
+
+  local terminal_buffer = require("configs.functions").is_buffer_exists(terminal_name)
+  if terminal_buffer then
+    vim.api.nvim_buf_delete(terminal_buffer, { force = true, unload = false })
+  end
   vim.cmd("botright " .. (opts.height or "15") .. "new")
   vim.fn.termopen(cmd .. " " .. vim.fn.join(args, " "), { cwd = opts.cwd })
+  vim.api.nvim_buf_set_name(0, terminal_name)
 
   if opts.focus_terminal then
     vim.cmd([[startinsert!]])
