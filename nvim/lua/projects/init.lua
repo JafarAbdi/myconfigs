2deb42965ca5e5030aefdf30c0e1592339ee8dcf
JafarAbdi <cafer.abdi@gmail.com>
2022-10-14 01:03:24 +0300

    Update to projects

--- nvim/lua/projects/init.lua
+++ nvim/lua/projects/init.lua
@@ -1,6 +1,27 @@
+local filetype = require("plenary.filetype")
+
 --- @class Project
---- @field lang string
+--- @field language string
+--- @field root_path string
 --- @field build_system "cargo" | "rustc" | "cmake" | "clang"
+local Project = {}
+
+--- @return Project
+function Project:new(opts)
+  setmetatable(opts, self)
+  self.__index = self
+  return opts
+end
+
+function Project:set()
+  vim.cmd.cd(self.root_path)
+  if self.build_system == "cmake" then
+    require("configs.cmake").cmake_project(self.root_path)
+  end
+end
+
+--- @param file string
+function Project:run(file) end
 
 local M = {}
 
@@ -12,11 +33,12 @@ M.projects = {}
 M.add_project = function(root_path, project)
   local p = M.projects[root_path]
   if p then
-    if p.lang ~= project.lang or p.build_system ~= p.build_system then
+    if p.language ~= project.language or p.build_system ~= p.build_system then
       vim.notify("Conflicting configurations for project '" .. root_path)
     end
     return
   end
+  project.root_path = root_path
   M.projects[root_path] = project
 end
 
@@ -29,13 +51,15 @@ end
 --- @param file_path string
 --- @return Project
 M.set_project = function(file_path)
-  for project_root_path, info in pairs(M.projects) do
-    if startswith(file_path, project_root_path) then
-      vim.cmd.cd(project_root_path)
-      if info.build_system == "cmake" then
-        require("configs.cmake").cmake_project(project_root_path)
-      end
-      return info
+  -- local type = filetype.detect(file_path)
+  for root_path, project in pairs(M.projects) do
+    if startswith(file_path, root_path) then
+      project:set()
+      -- vim.cmd.cd(root_path)
+      -- if project.build_system == "cmake" then
+      --   require("configs.cmake").cmake_project(root_path)
+      -- end
+      -- return project
     end
   end
 end
