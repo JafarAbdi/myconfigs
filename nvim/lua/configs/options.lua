4ffb6916fc66fec26b9946ad62cd63bf5665f4a1
JafarAbdi <cafer.abdi@gmail.com>
2022-10-13 23:07:57 +0300

    NVim: Add netrw back

--- nvim/lua/configs/options.lua
+++ nvim/lua/configs/options.lua
@@ -53,6 +53,15 @@ g.diagnostic_virtual_text = false
 
 vim.opt.iskeyword:append("-")
 
+g.netrw_list_hide = [[,\(^\|\s\s\)\zs\.\S\+]]
+g.netrw_altv = 1
+g.netrw_keepdir = 0
+g.netrw_winsize = 20
+g.netrw_localcopydircmd = "cp -r"
+g.netrw_liststyle = 3
+g.netrw_banner = 0
+g.netrw_bufsettings = "signcolumn=no noma nomod nu nobl nowrap ro"
+
 vim.filetype.add({
   extension = {
     launch = "xml",
