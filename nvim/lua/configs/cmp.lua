762f2ac33208daa6279a959f107bfcebbdfcd382
JafarAbdi <cafer.abdi@gmail.com>
2022-11-16 23:57:18 +0300

    NVim: Use all buffers for completion

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -60,12 +60,13 @@ cmp.setup({
       max_item_count = 20,
       option = {
         get_bufnrs = function()
-          local buf = vim.api.nvim_get_current_buf()
-          local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
-          if byte_size > 1024 * 1024 then -- 1 Megabyte max
-            return {}
-          end
-          return { buf }
+          return vim.api.nvim_list_bufs()
+          -- local buf = vim.api.nvim_get_current_buf()
+          -- local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+          -- if byte_size > 1024 * 1024 then -- 1 Megabyte max
+          --   return {}
+          -- end
+          -- return { buf }
         end,
       },
     },
