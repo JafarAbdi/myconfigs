ec57ec8ec6f998f95f379cd708e4eccd5798cf70
JafarAbdi <cafer.abdi@gmail.com>
2022-11-19 00:39:00 +0300

    Remove having tmux by default

--- fish/config.fish
+++ fish/config.fish
@@ -9,13 +9,6 @@ set -x HISTIGNORE 'pwd:exit:fg:bg:top:clear:history:ls:uptime:df'
 set -x HISTSIZE 100000
 set -x HISTFILESIZE 100000
 
-
-if status is-interactive
-and not set -q TMUX
-and command -v tmux &> /dev/null
-  exec tmux new-session -s %self
-end
-
 if test -d $HOME/myconfigs # Host machine case
   set -x MYCONFIGS_DIR ~/myconfigs
 else if test -d /root/myconfigs # Docker image case
