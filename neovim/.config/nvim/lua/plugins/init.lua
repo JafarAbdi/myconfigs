7777c924b9ad85bac1373ce0e6ef978d95b83db4
JafarAbdi <jafar.uruc@gmail.com>
2023-04-12 03:31:05 +0300

    NVim: Fix semantic tokens highlight groups

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -22,6 +22,29 @@ return {
     config = function()
       -- load the colorscheme here
       vim.cmd.colorscheme("onedark")
+      local links = {
+        ["@lsp.type.namespace"] = "@namespace",
+        ["@lsp.type.type"] = "@type",
+        ["@lsp.type.class"] = "@type",
+        ["@lsp.type.enum"] = "@type",
+        ["@lsp.type.interface"] = "@type",
+        ["@lsp.type.struct"] = "@type",
+        ["@lsp.type.parameter"] = "@parameter",
+        ["@lsp.type.variable"] = "@variable",
+        ["@lsp.type.property"] = "@property",
+        ["@lsp.type.enumMember"] = "@constant",
+        ["@lsp.type.function"] = "@function",
+        ["@lsp.type.method"] = "@method",
+        ["@lsp.type.macro"] = "@macro",
+        ["@lsp.type.decorator"] = "@function",
+        ["@lsp.type.keyword"] = "@keyword",
+        ["@lsp.type.operator"] = "@operator",
+        ["@lsp.type.string"] = "@string",
+        ["@lsp.type.number"] = "@number",
+      }
+      for semantic_tokens_group, treesitter_group in pairs(links) do
+        vim.api.nvim_set_hl(0, semantic_tokens_group, { link = treesitter_group, default = true })
+      end
     end,
   },
 }
