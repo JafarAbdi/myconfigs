d13baf9b422c02d54605f1fa382252d58d62342a
JafarAbdi <jafar.uruc@gmail.com>
2023-08-25 22:47:12 +0100

    Run lua lint

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -328,7 +328,7 @@ end
 
 config.exec_domains = exec_domains
 
-wezterm.on("augment-command-palette", function(window, pane)
+wezterm.on("augment-command-palette", function(_, _)
   return {
     {
       brief = "Rename workspace",
@@ -336,7 +336,7 @@ wezterm.on("augment-command-palette", function(window, pane)
 
       action = act.PromptInputLine({
         description = "Enter new name for workspace",
-        action = wezterm.action_callback(function(window, pane, line)
+        action = wezterm.action_callback(function(_, _, line)
           wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
         end),
       }),
@@ -347,7 +347,7 @@ wezterm.on("augment-command-palette", function(window, pane)
 
       action = act.PromptInputLine({
         description = "Enter new name for tab",
-        action = wezterm.action_callback(function(window, pane, line)
+        action = wezterm.action_callback(function(window, _, line)
           if line then
             window:active_tab():set_title(line)
           end
