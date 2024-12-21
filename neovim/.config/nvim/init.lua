1542ed7411719262c69ade79eeda39924011b5c0
JafarAbdi <jafar.uruc@gmail.com>
2024-12-21 18:47:18 +0300

    Add treesitter support

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1507,6 +1507,113 @@ require("lazy").setup({
       })
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "VeryLazy" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "c_sharp",
+        "cmake",
+        "cpp",
+        "dockerfile",
+        "fish",
+        "html",
+        "xml",
+        "http",
+        "javascript",
+        "json",
+        "lua",
+        "comment",
+        "make",
+        "markdown",
+        "ninja",
+        "proto",
+        "python",
+        "query",
+        "rst",
+        "rust",
+        "toml",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+        "zig",
+      },
+      highlight = {
+        enable = true,
+        disable = function(lang, buf)
+          return (lang == "html")
+            -- Disable highlighting for files without a filetype
+            or (vim.api.nvim_buf_get_option(buf, "filetype") == "")
+        end,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<A-w>",
+          node_incremental = "<A-w>",
+          scope_incremental = "<A-e>",
+          node_decremental = "<A-S-w>",
+        },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["ap"] = "@parameter.outer",
+            ["ip"] = "@parameter.inner",
+            ["ao"] = "@conditional.outer",
+            ["io"] = "@conditional.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]c"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]C"] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[c"] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[C"] = "@class.outer",
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 }, {
   defaults = {
     lazy = true, -- every plugin is lazy-loaded by default
@@ -1591,6 +1698,7 @@ end
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.treesitter.language.register("xml", { "xacro", "urdf" })
 vim.filetype.add({
   extension = {
     launch = "xml",
