7973e74c5ce7658ce449351759d264e3288f4f66
JafarAbdi <jafar.uruc@gmail.com>
2026-02-13 11:08:28 +0000

    Bring back docker stuff

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -45,7 +45,6 @@ config.show_new_tab_button_in_tab_bar = false
 config.tab_max_width = 25
 config.disable_default_key_bindings = true
 config.default_gui_startup_args = { "start", "--always-new-process" }
-
 config.window_padding = {
   left = 0,
   right = 0,
@@ -236,6 +235,138 @@ config.font =
   wezterm.font({ family = "JetBrains Mono", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } })
 config.term = "wezterm"
 
+-- Docker domains
+local split_lines = function(s)
+  local lines = {}
+  for line in s:gmatch("[^\n]+") do
+    table.insert(lines, line)
+  end
+  return lines
+end
+
+local docker_list = function()
+  local success, stdout, _ =
+    wezterm.run_child_process({ "docker", "container", "ls", "--format", "{{.Names}}" })
+  if success then
+    return split_lines(stdout)
+  end
+  return {}
+end
+
+local make_docker_fixup_func = function(id)
+  return function(cmd)
+    local _, stdout, _ = wezterm.run_child_process({
+      "docker",
+      "exec",
+      id,
+      "bash",
+      "-c",
+      string.format(
+        [[awk -F: -v user="%s" '$1 == user {print $NF}' /etc/passwd]],
+        os.getenv("USER")
+      ),
+    })
+    local shell = split_lines(stdout)[1]
+    -- TODO: Better detection for fish
+    if shell == "/usr/sbin/nologin" then
+      shell = "fish"
+    end
+    cmd.args = cmd.args or { shell }
+    local wrapped = {
+      "docker",
+      "exec",
+      "-it",
+      "--user",
+      os.getenv("USER"),
+      id,
+    }
+    for _, arg in ipairs(cmd.args) do
+      table.insert(wrapped, arg)
+    end
+
+    cmd.args = wrapped
+    return cmd
+  end
+end
+
+local make_docker_label_func = function(id)
+  return function(_)
+    local success, stdout, _ = wezterm.run_child_process({
+      "docker",
+      "container",
+      "inspect",
+      "-f",
+      "{{.State.Status}}",
+      id,
+    })
+    if success then
+      local result = split_lines(stdout)[1]
+      if result == "running" then
+        return wezterm.format({
+          { Foreground = { AnsiColor = "Green" } },
+          { Text = "Running " .. id },
+        })
+      elseif result == "exited" then
+        return wezterm.format({
+          { Foreground = { AnsiColor = "Red" } },
+          { Text = "Stopped " .. id },
+        })
+      else
+        return wezterm.format({
+          { Foreground = { AnsiColor = "Yellow" } },
+          { Text = result .. " " .. id },
+        })
+      end
+    end
+    return wezterm.format({
+      { Foreground = { AnsiColor = "Red" } },
+      { Text = "Can't get state for " .. id },
+    })
+  end
+end
+
+local exec_domains = {}
+for _, name in pairs(docker_list()) do
+  table.insert(
+    exec_domains,
+    wezterm.exec_domain(
+      "docker: " .. name,
+      make_docker_fixup_func(name),
+      make_docker_label_func(name)
+    )
+  )
+end
+
+config.exec_domains = exec_domains
+
+wezterm.on("augment-command-palette", function(_, _)
+  return {
+    {
+      brief = "Rename workspace",
+      icon = "md_rename_box",
+
+      action = act.PromptInputLine({
+        description = "Enter new name for workspace",
+        action = wezterm.action_callback(function(_, _, line)
+          wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+        end),
+      }),
+    },
+    {
+      brief = "Rename tab",
+      icon = "md_rename_box",
+
+      action = act.PromptInputLine({
+        description = "Enter new name for tab",
+        action = wezterm.action_callback(function(window, _, line)
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      }),
+    },
+  }
+end)
 config.check_for_updates = false
 
 wezterm.on("bell", function(window, pane)
